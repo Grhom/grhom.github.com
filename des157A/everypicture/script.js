c5f6f6ff567869d8bbc36e2ca0c56b8f0869e61e
Gennifer Hom <grhom@ucdavis.edu>
2021-11-07 07:26:47 -0800

    update validation links

---
+++ des157A/everypicture/script.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  // make a div and have the images loop as you scroll
+  var imgs = [
+    "images/chb.png",
+    "images/chb1.png",
+    "images/chb2.png",
+    "images/chb3.png",
+  ];
+})();
