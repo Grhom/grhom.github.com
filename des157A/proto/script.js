e7edbaaea8bc8ab88829d6e3940a04d8b7e85a71
Gennifer Hom <grhom@ucdavis.edu>
2021-11-29 04:39:44 -0800

    proto update

--- des157A/proto/script.js
+++ des157A/proto/script.js
@@ -16,12 +16,24 @@
   // corgi images
   var happycorgi = document.getElementById("happycorgi");
   var angrycorgi = document.getElementById("angrycorgi");
+  var wincorgi = document.getElementById("wincorgi");
   // variables for dice animation
   var DiceOne = document.getElementById("dice1");
   var DiceTwo = document.getElementById("dice2");
   var ComeOut = document.getElementById("roll");
 
-  var win = document.getElementById("header");
+  var win = document.getElementById("win");
+
+  // sound variables
+  var bark = document.getElementById("bark");
+  var happy = document.getElementById("happy");
+  var dicesound = document.getElementById("rolling");
+  // icons
+  var playsound = document.getElementById("playsound");
+  var mute = document.getElementById("mute");
+  var volume = document.getElementById("volume");
+  var help = document.getElementById("help");
+  var helpcard = document.getElementById("helpcard");
 
   var gameData = {
     players: ["player1", "player2"],
@@ -32,6 +44,44 @@
     index: 0,
     gameEnd: 29,
   };
+
+  // bowl images
+  var bone1 = document.getElementsByClassName("bone1");
+  var bone2 = document.getElementsByClassName("bone2");
+  var bones = [bone1, bone2];
+
+  help.addEventListener("click", function () {
+    helpcard.hidden = false;
+  });
+  volume.addEventListener("change", function (e) {
+    bark.volume = e.currentTarget.value / 100;
+    happy.volume = e.currentTarget.value / 100;
+    dicesound.volume = e.currentTarget.value / 100;
+    unmute();
+  });
+
+  playsound.addEventListener("click", function () {
+    // show mute icon
+    mute.hidden = false;
+    // mute volume slider
+    volume.value = 0;
+    // hide play sounds
+    playsound.hidden = true;
+
+    // turn off sounds
+    bark.volume = 0;
+    happy.volume = 0;
+    dicesound.volume = 0;
+  });
+
+  function unmute() {
+    // show sound icon
+    mute.hidden = true;
+    // show play sounds
+    playsound.hidden = false;
+  }
+
+  mute.addEventListener("click", unmute);
 
   startGame.addEventListener("click", function () {
     let name1 = title1.value || gameData.players[0];
@@ -48,7 +98,7 @@
 
     ComeOut.hidden = false;
     gameData.index = Math.round(Math.random());
-    gameControl.innerHTML += `<button id="quit">QUIT?</button>`;
+    gameControl.innerHTML += `<button id="quit">RESTART</button>`;
 
     document.getElementById("quit").addEventListener("click", function () {
       location.reload();
@@ -106,6 +156,7 @@
     gameData.roll1 = rolls.dice1;
     gameData.roll2 = rolls.dice2;
     gameData.rollSum = rolls.dice1 + rolls.dice2;
+    dicesound.play();
 
     // if two 1's are rolled
     if (gameData.rollSum === 2) {
@@ -138,6 +189,7 @@
       // }</p>`;
       happycorgi.hidden = true;
       angrycorgi.hidden = false;
+      bark.play();
       if (gameData.index == 0) {
         player1.classList.add("highlight");
         player1.classList.remove("nohighlight");
@@ -156,7 +208,16 @@
     else {
       happycorgi.hidden = false;
       angrycorgi.hidden = true;
+
       gameData.score[gameData.index] += gameData.rollSum;
+
+      for (let i = 0; i < bone1.length - 1; i++) {
+        if (!bones[gameData.index][i].hidden) {
+          bones[gameData.index][i].hidden = true;
+          bones[gameData.index][i + 1].hidden = false;
+          break;
+        }
+      }
 
       // document.getElementById("pass").addEventListener("click", function () {
       //   gameData.index ? (gameData.index = 0) : (gameData.index = 1);
@@ -168,8 +229,15 @@
     function checkWinningCondition() {
       if (gameData.score[gameData.index] > gameData.gameEnd) {
         // win.innerHTML=`${} Wins!`
-        win.innerHTML = `<h1>${gameData.players[gameData.index]} wins!</h1>`;
+        win.innerHTML = `${gameData.players[gameData.index]} wins!`;
         console.log("win!");
+        setTimeout(() => {
+          happy.play();
+        }, 500);
+        ComeOut.hidden = true;
+        happycorgi.hidden = true;
+        angrycorgi.hidden = true;
+        wincorgi.hidden = false;
       } else {
         // show current score
         // score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}:${gameData.score[0]}</strong> and <strong>The score is currently <strong>${gameData.players[1]}:${gameData.score[1]}</strong></p>`;
